7aa99bb4bf13b21107574bfb6e22196de210d780
Amor-Madhkour <79710817+Amor-Madhkour@users.noreply.github.com>
2021-03-10 11:21:44 +0100

    print example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
+/*
 package org.example;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 /**
  * Hello world!
  *
- */
+ //
 public class App 
 {
+    ByteArrayOutputStream baus;
+    ByteArrayInputStream bais;
+    public App()
+    {
+
+        baus = new ByteArrayOutputStream();
+        baus.write(3);
+
+        bais = new ByteArrayInputStream(baus.toByteArray());
+
+        bais.read();
+    }
+
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 }
+
+*/
+
+
+
+
